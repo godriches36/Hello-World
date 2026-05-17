a572d272ad7bfdeed1b006e5e45fd10e11b915fc
Sun God Riches <godriches072@gmail.com>
2026-05-17 15:55:10 +0100

    Refactor worker.js to remove fetch and update body
    Removed the fetch function and updated the response body structure.

--- worker.js
+++ worker.js
@@ -1,37 +1,6 @@
-export default {
-  async fetch(request, env) {
-    const INFURA_URL = `https://mainnet.infura.io/v3/${env.INFURA_ID}`;
-
-    // 05Fa Protocol: Direct Handshake with the Blockchain
-    const response = await fetch(INFURA_URL, {
-      method: 'POST',
-      body: JSON.stringify({
-        jsonrpc: "2.0",
-        method: "eth_blockNumber",
-        params: [],
-        id: 1
-      }),
-    });
-
-    const data = await response.json();
-    const blockNumber = parseInt(data.result, 16);
-
-    // This is the 1% Logic controlling the output
-    const body = {
-      status: "SOVEREIGN_ACTIVE",
-      protocol: "05Fa",
-      architect: "0x8d08",
-      frequency: "1.25T_NAIRA_INVERSION",
-      live_block: blockNumber,
-      message: "Agbon Kingdom Infrastructure: Online"
-    };
-
-    return new Response(JSON.stringify(body), {
-      headers: { 
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*" // Allows ANBSN-OS to read this data
-      },
-    });
-  },
+const body = {
+  status: "SOVEREIGN_ACTIVE",
+  protocol: "05Fa",
+  origin: "ANBSN-SOVEREIGN OS", // Updated name
+  message: "Agbon Kingdom Infrastructure: Online"
 };
-

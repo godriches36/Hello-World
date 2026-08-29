443279ac8c360d47ff1f8fc1ca5e978819da7a55
Sun God Riches <godriches072@gmail.com>
2026-08-29 03:11:20 +0100

    Fix comment formatting in worker.js

--- worker.js
+++ worker.js
@@ -1,6 +1,6 @@
 const body = {
   status: "SOVEREIGN_ACTIVE",
   protocol: "05Fa",
-  origin: "ANBSN-SOVEREIGN OS", // Updated name
+  origin: "ANBS-SOVEREIGN OS", // Updated name
   message: "Agbon Kingdom Infrastructure: Online"
 };
